dfd216b6887ccb1e8e461e07cca495e60ce0cc0d
IvanK <mr.koldamasov@mail.ru>
2022-10-30 16:27:09 +0300

    двумерный массив. Найдите элементы, // у которых оба индекса нечётные, // и замените эти элементы на их квадраты

---
+++ Ex049/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса нечётные,
+// и замените эти элементы на их квадраты.
+
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными 
+// целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j += 2)
+            matrix[i, j] *= matrix[i, j];
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+PrintMatrix(matrix);

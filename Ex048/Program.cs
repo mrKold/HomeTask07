2fcb6400e071acba9166c20a69dd00ae4f677c9e
IvanK <mr.koldamasov@mail.ru>
2022-10-30 16:02:51 +0300

    двумерный массив размера m на n, каждый элемент в массиве находится по формуле: A = m+n

---
+++ Ex048/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите колличество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите колличество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
